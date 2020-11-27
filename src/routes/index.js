6f7cc3cef3b7f2c898b9cc52532fada1a39b464d
Zhihao Zhou <redickhao@gmail.com>
2020-11-26 16:58:07 -0800

    front

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 router.get('/storyPreview', (req, res) => {
-  res.render('storyPreview', { title: 'Stroy preview ' });
+  res.render('storyPreview', {
+    title: 'Stroy preview '
+  });
 });
 
 export default router;
